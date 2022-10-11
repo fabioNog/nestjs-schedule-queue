1f250d5aad6b95a20ea821b103fddea5323fe2b0
fabiocarmovilanova <fabio.carmo@vilanova.com.br>
2022-10-10 22:03:57 -0300

    Create a res send

--- src/courses/courses.controller.ts
+++ src/courses/courses.controller.ts
@@ -1,11 +1,18 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Res } from '@nestjs/common';
+import { response } from 'express';
 
 @Controller('courses')
 export class CoursesController {
-    @Get()
+/*    @Get()
     findAll(){
         return 'Listagem de Cursos';
+    }*/
+
+    @Get()
+    findAll(@Res() response){
+        return response.status(200).send('Listagem de Cusos');
     }
+    
 
     @Get(':id')
     findOne(@Param() params){
