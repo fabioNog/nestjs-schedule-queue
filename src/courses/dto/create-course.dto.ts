a4d98d0d6912caa81e21dc82fef5653fb3d7228d
fabiocarmovilanova <fabio.carmo@vilanova.com.br>
2022-12-04 20:35:37 -0300

    Insert validator

--- src/courses/dto/create-course.dto.ts
+++ src/courses/dto/create-course.dto.ts
@@ -1,6 +1,11 @@
+import {IsNumber, IsString} from 'class-validator';
 export class CreateCourseDto {
+    @IsNumber()
     readonly id: number
+    @IsString()
     readonly name: string;
+    @IsString()
     readonly description: string;
+    @IsString({each: true})
     readonly tags: string[];
 }
